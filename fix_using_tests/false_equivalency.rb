af488a806d4fe80f36255e4e90927280be18d2ef
Fred C <canofred@gmail.com>
2020-03-31 16:06:05 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,24 +1,32 @@
-# don't forget to add: require 'pry'
+require 'pry'
 
 def get_user_input
+  # binding.pry
   gets.chomp
 end
 
+
 def prompt_user
+# binding.pry
   puts "What would you like to do?"
   puts "1.) Eat a hamburger."
   puts "2.) Eat a ham."
 end
 
+
 def selection(num)
-  if num = 1
+  if num == 1
+    # binding.pry
      "YUM YUM MUNCH MUNCH MUNCH"
-  elsif num = 2
+  elsif num == 2
      "HAM HAM HAM IN MY TUMMY"
   end
 end
 
+
 def runner
+  # binding.pry
   prompt_user
   selection(get_user_input)
 end
+
--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,4 +1,4 @@
-# don't forget to add: require 'pry'
+require 'pry'
 
 def generate_star_date
   (rand(100000) + 400000) / 10.0
@@ -6,10 +6,25 @@ end
 
 
 def state_log(star_date)
+  binding.pry
   "Captain's Log, star date #{star_date}."
 end
+state_log(2334)
+
 
 def engage
-  puts state_log(date)
+  # binding.pry
   date = generate_star_date
+  puts state_log(date)
 end
+
+
+
+
+# original
+# def engage
+#   puts state_log(date)
+#   date = generate_star_date
+# end
+
+
